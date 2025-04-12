bcf5ea726472887dbc698e8c25026baff7dba104
José Edmundo González Herrera <34552061+jgonzalezh78@users.noreply.github.com>
2025-04-11 18:38:09 -0600

    [GFTCodeFixer]: Update on src/main/java/com/scalesec/vulnado/CowController.java

--- src/main/java/com/scalesec/vulnado/CowController.java
+++ src/main/java/com/scalesec/vulnado/CowController.java
@@ -3,10 +3,9 @@ package com.scalesec.vulnado;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.*;
 
-@RestController
 @EnableAutoConfiguration
 public class CowController {
-    @RequestMapping(value = "/cowsay", method = RequestMethod.GET)
+    @RequestMapping(value = "/cowsay", method = {RequestMethod.GET, RequestMethod.POST})
     String cowsay(@RequestParam(defaultValue = "I love Linux!") String input) {
         return Cowsay.run(input);
     }
